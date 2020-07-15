26f1ec449c7f8dbc82273022d12f579f8e0c616e
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-07-15 14:11:11 -0700

    add remove functionality to list items
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- ToDoList/Main.cs
+++ ToDoList/Main.cs
@@ -9,15 +9,19 @@ namespace ToDoList
     public static void Main()
     {
       Console.WriteLine("Welcome to the to do list!");
-      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-      string addOrView01 = (Console.ReadLine()).ToLower();
-      if (addOrView01 == "add")
+      Console.WriteLine("Would you like to add an item to your list, remove an item from your list or view your list? (add/remove/view)");
+      string choice01 = (Console.ReadLine()).ToLower();
+      if (choice01 == "add")
       {
         Add();
       }
-      else if (addOrView01 == "view")
+      else if (choice01 == "view")
       {
         View();
+      }
+      else if (choice01 == "remove")
+      {
+        FindAndRemove();
       }
       else
       {
@@ -30,15 +34,19 @@ namespace ToDoList
       string description = Console.ReadLine();
       Item newItem = new Item(description);
       Console.WriteLine(description + " has been added to your list.");
-      Console.WriteLine("Would you like to add to your list or view your list? (add/view)");
-      string addOrView02 = (Console.ReadLine()).ToLower();
-      if (addOrView02 == "add")
+      Console.WriteLine("Would you like to add an item to your list, remove an item from your list or view your list? (add/remove/view)");
+      string choice02 = (Console.ReadLine()).ToLower();
+      if (choice02 == "add")
       {
         Add();
       }
-      else if (addOrView02 == "view")
+      else if (choice02 == "view")
       {
         View();
+      }
+      else if (choice02 == "remove")
+      {
+        FindAndRemove();
       }
       else
       {
@@ -49,17 +57,54 @@ namespace ToDoList
     {
       Console.WriteLine("Your To Do list:");
       List<Item> result = Item.GetAll();
-      foreach (Item thisItem in result)
+      if (result.Count >= 1 )
       {
-        Console.WriteLine(thisItem.Description);
+        foreach (Item thisItem in result)
+        {
+          Console.WriteLine(thisItem.Description);
+        }
+      }
+      else
+      {
+        Console.WriteLine("Your To Do List is empty.  Congratulations!");
       }
       Console.WriteLine("Would you like to add more items to your list or are you finished? (add/end");
-      string addOrEnd = (Console.ReadLine()).ToLower();
-      if (addOrEnd == add)
+      string choice03 = (Console.ReadLine()).ToLower();
+      if (choice03 == "add")
       {
         Add();
       }
-      else if (addOrEnd == end)
+      else if (choice03 == "end")
+      {
+        Console.WriteLine("Goodbye!");
+      }
+      else
+      {
+        Main();
+      }
+    }
+    public static void FindAndRemove()
+    {
+      Console.WriteLine("Enter the description of the task you would like to remove:");
+      string locate = Console.ReadLine();
+      int found = Item.FindItem(locate);
+      Item.RemoveItem(found);
+      Console.WriteLine(locate + " has been removed from your To Do list.");
+      Console.WriteLine("Would you like to add an item to your list, remove an item from your list or view your list? (add/remove/view/end)");
+      string choice04 = (Console.ReadLine()).ToLower();
+      if (choice04 == "add")
+      {
+        Add();
+      }
+      else if (choice04 == "view")
+      {
+        View();
+      }
+      else if (choice04 == "remove")
+      {
+        FindAndRemove();
+      }
+      else if (choice04 == "end")
       {
         Console.WriteLine("Goodbye!");
       }
--- ToDoList/Models/List.cs
+++ ToDoList/Models/List.cs
@@ -4,24 +4,34 @@ namespace ToDoList.Models
 {
   public class Item
   {
+    public string Description { get; set; }
 
-  public string Description { get; set; }
-  private static List<Item> _instances = new List<Item> {};
+    private static List<Item> _instances = new List<Item> {};
 
-  public Item(string description)
-  {
-    Description = description;
-    _instances.Add(this);
-  }
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
 
-  public static List<Item> GetAll()
-  {
-    return _instances;
-  }
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
 
     public static void ClearAll()
-  {
-    _instances.Clear();
-  }
+    {
+      _instances.Clear();
+    }
+
+    public static int FindItem(string description)
+    {
+      return _instances.IndexOf(_instances.Find(i => i.Description == description));
+    }
+
+    public static void RemoveItem(int found)
+    {    
+      _instances.RemoveAt(found);
+    }
   }
 }
